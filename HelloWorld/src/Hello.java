9141b639f25a0c2a7609bcc001ee8574196196ac
Ferhat <losandess258@gmail.com>
2024-04-26 12:47:06 -0400

    Yukleniyor

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+String ss="5";
+        String b = ss + ss;
+        int a = 10, c = 20;
+        a=c;
+        c=10;
+        System.out.println("c="+c );
+        System.out.println("a="+a );
+        System.out.println("b="+b);
+
+    }
+}
---
+++ HelloWorld/src/menu/Menuclass.java
@@ -0,0 +1,13 @@
+package menu;
+public class Menuclass {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        String ss="5";
+        ss=ss+ss;
+        System.out.println("ss="+ss);
+
+
+    }
+
+}
